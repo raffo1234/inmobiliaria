0d22eb34121de02c4f5de375a85c23dec80635fe
Rafael <rhmcord@gmail.com>
2025-03-28 22:11:35 -0500

    feat: add the Type table
    - Display Type information from the table

--- src/components/PropertiesList.tsx
+++ src/components/PropertiesList.tsx
@@ -5,8 +5,18 @@ import hero from "../assets/hero.jpg";
 const fetcher = async () => {
   const { data, error } = await supabase
     .from("property")
-    .select("*")
+    .select(
+      `
+      id,
+      title,
+      type (
+        id,
+        name
+      )
+    `
+    )
     .order("created_at", { ascending: false });
+
   if (error) throw error;
   return data;
 };
@@ -17,17 +27,25 @@ export default function PropertiesList() {
     error,
     isLoading,
   } = useSWR("properties", fetcher);
-  console.log(properties, "properties");
-  return properties.map(({ id }) => {
+
+  return properties.map(({ id, title }) => {
     return (
       <article key={id}>
         <a href="/">
           <img
             src={hero.src}
-            className="w-full h-[320px] object-cover object-top rounded-lg"
+            className="w-full h-[320px] object-cover object-top rounded-lg mb-4"
             alt="Inmobiliaria"
           />
         </a>
+        <div className="flex items-center gap-2 mb-2">
+          <div className="w-5 h-5 rounded-full bg-black"></div>
+          <h3>
+            <a href="/" className="font-semibold">
+              {title}
+            </a>
+          </h3>
+        </div>
       </article>
     );
   });
