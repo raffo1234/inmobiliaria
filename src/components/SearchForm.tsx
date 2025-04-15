1a3ef41592fe9c2ba358eb402d227f2c00296f46
Rafael <rhmcord@gmail.com>
2025-04-15 13:54:07 -0500

    feat: improve search functionality
    t # Please enter the commit message for your changes. Lines starting

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,47 +1,89 @@
-import FilterDropdown from "@components/FilterDropdown";
 import { Icon } from "@iconify/react";
-import { supabase } from "@lib/supabase";
+import { PropertyType } from "@types/propertyState";
+import { useEffect, useState } from "react";
 import { useForm, type SubmitHandler } from "react-hook-form";
 
 type Inputs = {
   type: string;
-  buscador: string;
+  keywords: string;
 };
 
-export default function SearchForm() {
-  const { reset, register, handleSubmit } = useForm<Inputs>({
+export default function SearchForm({
+  pathnameArray,
+}: {
+  pathnameArray?: string[];
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [typeInput, setTypeInput] = useState<string>(
+    (pathnameArray?.at(0)?.toUpperCase() as PropertyType) ||
+      PropertyType.APARTMENT
+  );
+  const { register, reset, handleSubmit } = useForm<Inputs>({
     mode: "onBlur",
   });
 
   const onSubmit: SubmitHandler<Inputs> = async (formData) => {
-    const keywords = encodeURIComponent(formData.buscador);
-    window.location.href = `/departamentos/${keywords}`;
+    const keywords = encodeURIComponent(formData.keywords);
+    window.location.href = `/${typeInput?.toLowerCase()}/${keywords}`;
   };
+
+  useEffect(() => {
+    if (pathnameArray) {
+      setTypeInput(pathnameArray.at(0)?.toUpperCase() as PropertyType);
+      if (pathnameArray[1]) {
+        reset({ keywords: decodeURI(pathnameArray[1]) });
+      }
+    }
+  }, []);
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="mb-20 w-full max-w-[600px] mx-auto"
+      className="mb-20 w-full max-w-[600px] mx-auto relative z-30"
     >
       <div className="w-full hover:bg-white hover:border-cyan-100 focus-within:bg-white focus-within:border-cyan-100 border-2 transition-colors duration-500 border-gray-100 flex items-center bg-gray-100 rounded-full p-1 gap-2">
-        <div className="relative group">
+        <div
+          onMouseEnter={() => setIsOpen(true)}
+          onMouseLeave={() => setIsOpen(false)}
+          className="relative group"
+        >
           <button
             type="button"
             className="h-[52px] pl-6 pr-3 bg-slate-200 font-semibold rounded-full flex items-center gap-1"
           >
-            <span>Depas</span>
-            <Icon
-              fontSize={24}
-              icon="material-symbols-light:chevron-left-rounded"
-              className="rotate-90 group-hover:-rotate-90 transition-transform duration-200"
-            />
+            <span>
+              {typeInput === PropertyType.APARTMENT ? "Depas" : "Casas"}
+            </span>
+            <div className="w-6">
+              <Icon
+                fontSize={24}
+                icon="material-symbols-light:chevron-left-rounded"
+                className={`${isOpen ? "rotate-90" : "-rotate-90"} transition-transform duration-200`}
+              />
+            </div>
           </button>
-          <div className="group-hover:translate-y-0 -translate-y-1 invisible group-hover:visible absolute group-hover:opacity-100 opacity-0 transition-all duration-500 top-full left-0 w-[120px] py-3">
+          <div
+            className={`${isOpen ? "translate-y-0 visible opacity-100" : "-translate-y-1 invisible opacity-0"} absolute transition-all duration-500 top-full left-0 w-[120px] py-3`}
+          >
             <div className="bg-white shadow-md rounded-[30px] p-2 flex flex-col">
-              <button className="w-full text-left px-5 py-3 hover:bg-slate-100 rounded-full">
+              <button
+                type="button"
+                onClick={() => {
+                  setTypeInput(PropertyType.APARTMENT);
+                  setIsOpen(false);
+                }}
+                className="w-full text-left px-5 py-3 hover:bg-slate-100 rounded-full"
+              >
                 Depas
               </button>
-              <button className="w-full text-left px-5 py-3 hover:bg-slate-100 rounded-full">
+              <button
+                onClick={() => {
+                  setTypeInput(PropertyType.HOUSE);
+                  setIsOpen(false);
+                }}
+                type="button"
+                className="w-full text-left px-5 py-3 hover:bg-slate-100 rounded-full"
+              >
                 Casas
               </button>
             </div>
@@ -50,7 +92,7 @@ export default function SearchForm() {
         <div className="flex-grow w-[100px]">
           <input
             type="search"
-            {...register("buscador")}
+            {...register("keywords")}
             className="w-full flex-grow placeholder:text-gray-400 border-transparent border-2 focus:outline-none py-3 bg-transparent"
             placeholder="Buscar ..."
           />
