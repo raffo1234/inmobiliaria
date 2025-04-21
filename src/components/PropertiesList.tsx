e8b24084988078db2e847b7970e63213a43a6770
rafaelmezac <rafaelmezac@gmail.com>
2025-04-21 20:33:52 +0000

    feat: move sql query to backend for the index pages

--- src/components/PropertiesList.tsx
+++ src/components/PropertiesList.tsx
@@ -1,50 +1,8 @@
-import { supabase } from "../lib/supabase";
-import useSWR from "swr";
 import { useEffect, useState } from "react";
 import Property from "./Property";
-import { PropertyState } from "@types/propertyState";
 import PropertyItem from "./PropertyItem";
 import PropertyDetail from "./PropertyDetail";
 import PropertiesGrid from "./PropertiesGrid";
-
-const fetcher = async () => {
-  const { data, error } = await supabase
-
-    .from("property")
-    .select(
-      `
-      id,
-      title,
-      description,
-      state,
-      user_id,
-      size,
-      delivery_at,
-      bathroom_count,
-      phase,
-      price,
-      bedroom_count,
-      company_id,
-      user!property_user_id_fkey (
-        id,
-        email,
-        name,
-        image_url
-      ),
-      company!property_company_id_fkey (
-        id,
-        name,
-        image_url,
-        logo_url
-      )
-    `
-    )
-    .eq("state", PropertyState.ACTIVE)
-    .order("created_at", { ascending: false });
-
-  if (error) throw error;
-  return data;
-};
 
 type Property = {
   id: string;
@@ -52,11 +10,11 @@ type Property = {
   description: string;
 };
 
-export default function PropertiesList({ userId }: { userId: string }) {
+export default function PropertiesList({ userId, properties }: { userId: string; properties: Property[] }) {
   const [showDetail, setShowDetail] = useState(false);
   const [currentHref, setCurrentHref] = useState("");
   const [propertyValue, setPropertyValue] = useState<Property>();
-  const { data: properties = [], isLoading } = useSWR("properties", fetcher);
+
 
   useEffect(() => {
     setCurrentHref(window.location.href);
@@ -83,7 +41,7 @@ export default function PropertiesList({ userId }: { userId: string }) {
               property={property}
               setShowDetail={setShowDetail}
               setPropertyValue={setPropertyValue}
-              isLoading={isLoading}
+              isLoading={false}
             />
           );
         })}
