bdbf0d9edec742942481c36dbe2bba8adcbd262e
Rafael <rafael@Rafaels-MacBook-Pro.local>
2025-04-22 18:28:43 -0500

    feat: improve styles

--- src/components/AddProperty.tsx
+++ src/components/AddProperty.tsx
@@ -41,10 +41,12 @@ export default function AddProperty({ userId }: { userId: string }) {
   };
 
   return (
+    <>
+    <h1 className="mb-8 font-semibold text-lg block">Agregar Inmueble</h1>
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex flex-col gap-4 mt-16">
+      <div className="flex flex-col gap-4">
         <fieldset>
-          <label htmlFor="title" className="block font-bold mb-2 font-manrope">
+          <label htmlFor="title" className="inline-block mb-2 text-sm">
             Titulo
           </label>
           <input
@@ -52,13 +54,13 @@ export default function AddProperty({ userId }: { userId: string }) {
             id="title"
             {...register("title")}
             required
-            className="w-full px-4 py-2 rounded-md border border-gray-200 focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500"
+            className="w-full px-3 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
           />
         </fieldset>
         <fieldset>
           <label
             htmlFor="description"
-            className="block mb-2.5 font-manrope font-bold"
+            className="inline-block mb-2 text-sm"
           >
             Descripcion
           </label>
@@ -67,13 +69,13 @@ export default function AddProperty({ userId }: { userId: string }) {
             id="decription"
             {...register("description")}
             required
-            className="w-full px-4 py-2 rounded-md border border-gray-200 focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500"
+            className="w-full px-3 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
           />
         </fieldset>
         <fieldset>
           <label
             htmlFor="location"
-            className="block mb-2.5 font-manrope font-bold"
+            className="inline-block mb-2 text-sm"
           >
             Ubicacion
           </label>
@@ -82,24 +84,21 @@ export default function AddProperty({ userId }: { userId: string }) {
             id="location"
             {...register("location")}
             required
-            className="w-full px-4 py-2 rounded-md border border-gray-200 focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500"
+            className="w-full px-3 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
           />
         </fieldset>
       </div>
-      <footer className="mt-10">
-        <a
-          href="/admin/property"
-          className="inline-block text-center font-bold text-sm flex-grow px-6 py-2 border-gray-200 bg-white border-width-2 border rounded-md"
-        >
-          Cancel
-        </a>
-        <button
-          type="submit"
-          className="inline-block px-6 flex-grow font-bold text-sm py-2 bg-blue-500 text-white rounded-md"
-        >
-          Agregar
-        </button>
-      </footer>
-    </form>
-  );
+      <footer className="mt-10 flex items-center gap-2">
+      <a
+        href="/admin/property"
+        className="font-semibold disabled:border-gray-100 disabled:bg-gray-100 inline-block py-3 px-10 bg-white text-sm border border-gray-100 rounded-lg transition-colors hover:border-gray-200 duration-500 active:border-gray-300"
+        >Cancelar</a>
+      <button
+        type="submit"
+        className="text-white font-semibold disabled:border-gray-100 disabled:bg-gray-100 inline-block py-3 px-10 text-sm bg-cyan-500 hover:bg-cyan-400 transition-colors duration-500 rounded-lg"
+        >Agregar</button>
+    </footer>
+      </form>
+    </>
+  )
 }
