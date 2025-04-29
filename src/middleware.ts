6dcb9e5c2dc974f8b24405cde96d337866199fe8
Rafael <rhmcord@gmail.com>
2025-04-29 08:49:38 -0500

    feat: remove await from sync database

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,21 @@
-import { syncUserWithDb } from '@lib/auth';
-import { defineMiddleware } from 'astro:middleware';
-import { getSession } from 'auth-astro/server';
+import { syncUserWithDb } from "@lib/auth";
+import { defineMiddleware } from "astro:middleware";
+import { getSession } from "auth-astro/server";
 
 export const onRequest = defineMiddleware(async (context, next) => {
   try {
-    
-    await syncUserWithDb(context)
-    
     const session = await getSession(context.request);
-    if ((context.url.pathname.startsWith("/admin") || context.url.pathname.includes("/admin/")) && !session?.user) {
+    if (
+      (context.url.pathname.startsWith("/admin") ||
+        context.url.pathname.includes("/admin/")) &&
+      !session?.user
+    ) {
       return context.redirect("/");
     }
-    return next()
+    syncUserWithDb(context);
+    return next();
   } catch (error) {
-    console.error("Error retrieving session in middleware:", error);    
+    console.error("Error retrieving session in middleware:", error);
   }
   return next();
 });
