1e559bbe1f0c1e9a5fd1cc0c7aca46906cb0eaa6
Rafael <rhmcord@gmail.com>
2025-04-30 18:08:09 -0500

    feat: add pagination to properties by company

--- src/components/PropertiesListByCompany.tsx
+++ src/components/PropertiesListByCompany.tsx
@@ -1,6 +1,12 @@
 import Property from "./Property";
 import PropertyItem from "./PropertyItem";
 import PropertiesGrid from "./PropertiesGrid";
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+import { propertyQuery } from "@queries/property";
+import { PropertyState } from "@types/propertyState";
+import { supabase } from "@lib/supabase";
+import InfiniteScrollSentinel from "./InfiniteScrollSentinel";
 
 type Property = {
   id: string;
@@ -8,25 +14,112 @@ type Property = {
   description: string;
 };
 
-export default function PropertiesListByCompany({
-  userId,
-  properties,
+const fetcherAllByCompany = async (companyId: string) => {
+  const { count, error } = await supabase
+    .from("property")
+    .select("id", { count: "exact", head: true })
+    .eq("state", PropertyState.ACTIVE)
+    .eq("company_id", companyId);
+  if (error) throw error;
+  return count;
+};
+
+const fetcher = async (
+  index: number,
+  pageSize: number,
+  companyId: string,
+): Promise<Property[]> => {
+  const { data, error } = await supabase
+    .from("property")
+    .select(propertyQuery)
+    .eq("company_id", companyId)
+    .eq("state", PropertyState.ACTIVE)
+    .order("created_at", { ascending: false })
+    .range(index * pageSize, index * pageSize + pageSize - 1);
+
+  if (error) throw error;
+  return data;
+};
+
+function Page({
+  page,
+  pageSize,
+  userEmail,
+  setIsLoadingMore,
+  companyId,
 }: {
-  userId: string;
-  properties: Property[];
+  page: number;
+  pageSize: number;
+  setIsLoadingMore: (value: boolean) => void;
+  userEmail: string | undefined | null;
+  companyId: string;
 }) {
+  const { data: properties, isLoading } = useSWR(
+    `properties-${page}-home`,
+    async () => await fetcher(page, pageSize, companyId),
+  );
+
+  useEffect(() => {
+    setIsLoadingMore(isLoading);
+  }, [properties?.length]);
+
+  return properties?.map((property) => (
+    <PropertyItem key={property.id} userEmail={userEmail} property={property} />
+  ));
+}
+
+export default function PropertiesListByCompany({
+  userEmail,
+  properties,
+  companyId,
+}: {
+  userEmail: string | null | undefined;
+  properties: Property[];
+  companyId: string;
+}) {
+  const pageSize = 4;
+  const [page, setPage] = useState(1);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+  const { data: total } = useSWR("total-properties-home-page", () =>
+    fetcherAllByCompany(companyId),
+  );
+  const totalPages = total ? Math.ceil((total - pageSize) / pageSize) : 0;
+  const pages = [];
+
+  for (let i = 1; i < page; i++) {
+    pages.push(
+      <Page
+        key={i}
+        page={i}
+        pageSize={pageSize}
+        setIsLoadingMore={setIsLoadingMore}
+        userEmail={userEmail}
+        companyId={companyId}
+      />,
+    );
+  }
+
   return (
-    <PropertiesGrid>
-      {properties.map((property) => {
-        return (
-          <PropertyItem
-            key={property.id}
-            userId={userId}
-            property={property}
-            isLoading={false}
-          />
-        );
-      })}
-    </PropertiesGrid>
+    <>
+      <PropertiesGrid>
+        {properties.map((property) => {
+          return (
+            <PropertyItem
+              key={property.id}
+              userEmail={userEmail}
+              property={property}
+              isLoading={false}
+            />
+          );
+        })}
+        {pages.map((page) => page)}
+      </PropertiesGrid>
+      {!isLoadingMore && page <= totalPages ? (
+        <InfiniteScrollSentinel
+          onElementVisible={() => setPage((prev) => prev + 1)}
+          loading={isLoadingMore}
+        />
+      ) : null}
+    </>
   );
 }
