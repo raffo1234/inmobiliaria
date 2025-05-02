987cb92b23e9dd2f9b803b2d1efebb25a572b251
Rafael <rhmcord@gmail.com>
2025-05-02 15:38:18 -0500

    feat: improve query
    - Remove unused attributes

--- src/queries/property.ts
+++ src/queries/property.ts
@@ -6,7 +6,6 @@ export const propertyQuery = `
   ),
   user!property_user_id_fkey (
     id,
-    email,
     name,
     image_url
   ),
@@ -21,24 +20,18 @@ export const favoriteQuery = `
   property(
     id,
     title,
-    description,
-    state,
     user_id,
-    created_at,
-    location,
     property_image (
       image_url
     ),
     user!property_user_id_fkey (
       id,
-      email,
       name,
       image_url
     ),
     company!property_company_id_fkey (
       id,
       name,
-      image_url,
       logo_url
     )
   )
